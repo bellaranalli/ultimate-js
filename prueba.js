c725d999b73435b8ec5c01c51ce48b33e745f223
Bella Ranalli <bela.ranalli@gmail.com>
2024-06-19 17:33:29 -0300

    fin de curso

--- prueba.js
+++ prueba.js
@@ -1,17 +1,3 @@
-let charvalue = " ";
-
-function FunctiontoSplitnumber(charvalue, position) {
-  if (!charvalue.trim() || /^\d$/.test(charvalue.trim())) {
-    if (position <= 5) {
-      charvalue = "0000" + charvalue.trim();
-    } else {
-      charvalue = charvalue.trim() + "00000000";
-    }
-  }
-
-  let firstpart = charvalue.substring(0, position);
-  let secondpart = charvalue.substring(position, charvalue.length);
-  return (firstpart + '-' + secondpart);
-}
-
-console.log(FunctiontoSplitnumber(charvalue, 5));
+let orden = '12';
+let ordenSRT = orden.toString();
+console.log(orden.padStart(8, '0'))
